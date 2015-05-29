56f3d183016416b3e21b70aa007203ec8dcf481b
Johnathan Free Wortley <00free@gmail.com>
2015-05-29 15:36:41 -0400

    Don't import everything

--- index.js
+++ index.js
@@ -6,15 +6,9 @@
 
 module.exports.after = after;
 module.exports.afterEach = afterEach;
-module.exports.assertions = assertions;
 module.exports.before = before;
 module.exports.beforeEach = beforeEach;
 module.exports.context = context;
 module.exports.describe = describe;
 module.exports.it = it;
-module.exports.pre = pre;
-module.exports.setup = setup;
-module.exports.suite = suite;
-module.exports.test = test;
-module.exports.testCase = testCase;
 
